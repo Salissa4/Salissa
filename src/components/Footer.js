8919eb5def909f87347a4e607cddefe0c94c9ede
Monahlissa <salissa4@gmail.com>
2022-12-26 20:23:22 -0600

    class name error corrected

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ export const Footer = () => {
     return (
         <footer className='footer'>
             <Container>
-                <Row className='align-item-center'>
-                    <Col sm={6}>
+                <Row className='align-items-center'>
+                    <Col size={12} sm={6}>
                         <img src={logo} alt='salissa'/>
                     </Col>
-                    <Col sm={6} className='text-center text-sm-end'>
+                    <Col size={12} sm={6} className='text-center text-sm-end'>
                         <div className='social-icon'>
-                            <a href='https://www.linkedin.com/in/salissah/'><img src={navIcon1} alt='linked in icon' /></a>
+                            <a href='https://www.linkedin.com/in/salissah/'><img src={navIcon1} alt='linkedin icon' /></a>
                             <a href='https://github.com/Salissa4'><img src={navIcon2} alt='github icon' /></a>
                         </div>
                         <p>Copyright 2022. All Rights Reserved</p>
