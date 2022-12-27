c17f8a4b37c210b47d91b01bde698ea891b62686
Monahlissa <salissa4@gmail.com>
2022-12-26 20:21:09 -0600

    add animation banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,12 +2,15 @@ import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import headerImg from '../assets/images/header-img.svg';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random()*100);
+    const [index, setIndex] = useState(1);
     const toRotate = [ 'Technical Project Manager', 'Web Developer' ];
     const period = 2000;
 
@@ -27,37 +30,49 @@ export const Banner = () => {
         setText(updatedText);
 
         if (isDeleting) {
-            setDelta(prevDelta => prevDelta /2)
+            setDelta(prevDelta => prevDelta /2);
         } 
 
         if (!isDeleting && updatedText === fullText) {
             setIsDeleting(true);
+            setIndex(prevIndex => prevIndex -1);
             setDelta(period);
         } else if ( isDeleting && updatedText === '') {
             setIsDeleting(false);
             setLoopNum(loopNum + 1);
+            setIndex(1);
             setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex +1);
         }
-
     }
 
     return (
         <section className='banner' id='home'>
             <Container>
-                <Row className='align-item-center'>
+                <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7}>
-                        <span className='tagline'>Welcome</span>
-                        <h1>{`Hi I'm webdecoded `}<span className='wrap'>{text}</span></h1>
-                        <p>Salissa has over 10 years of experience streamlining operations and timelines to help organizations achieve KPI goals and maximize client satisfaction. She is an adaptable self-starter who takes initiative and facilitates collaboration, augments technical skill with creativity, and maintains attention to detail while focusing on the big picture.</p>
-                        <button onClick={() => console.log('connect')}>Connect<ArrowRightCircle size={25}/></button>
+                        <TrackVisibility>
+                        {({ isVisible }) => 
+                            <div className={ isVisible ? 'animate__animated animate__fadeIn' : ''}>
+                                <span className='tagline'>Welcome</span>
+                                <h1>{`Salissa`}<span className='txt-rotate' dataPeriod='1000' data-rotate='[ "Technical Project Manager", "Web Developer"]'><span className='wrap'>{text}</span></span></h1>
+                                <p>Salissa has over 10 years of experience streamlining operations and timelines to help organizations achieve KPI goals and maximize client satisfaction. She is an adaptable self-starter who takes initiative and facilitates collaboration, augments technical skill with creativity, and maintains attention to detail while focusing on the big picture.</p>
+                                <button onClick={() => console.log('connect')}>Connect<ArrowRightCircle size={25}/></button>
+                            </div>}
+                        </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt='header img' />
+                        <TrackVisibility>
+                            {({ isVisible }) =>
+                            <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                                <img src={headerImg} alt='header img' />
+                            </div>}
+                        </TrackVisibility>
                     </Col>
                 </Row>
             </Container>
         </section>
     )
-
 }
 
