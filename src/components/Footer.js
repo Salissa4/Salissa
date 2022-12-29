43c3c3c84d51dc1671f74ef52fbf4699b5f43b02
Monahlissa <salissa4@gmail.com>
2022-12-28 18:55:23 -0600

    remov dep, chg imgs icons, add resume pdf.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import sal2 from '../assets/images/sal2.svg';
 import navIcon1 from '../assets/images/nav-icon1.svg';
-import github from '../assets/images/github.png';
+import git2 from '../assets/images/git2.png';
+import Pdf from '../assets/docs/SH.pdf';
+import resume from '../assets/images/resume.png';
+import email from '../assets/images/email.png'
 
 export default function Footer () {
     return (
@@ -14,7 +17,9 @@ export default function Footer () {
                     <Col size={12} sm={6} className='text-center text-sm-end'>
                         <div className='social-icon'>
                             <a href='https://www.linkedin.com/in/salissah/'><img src={navIcon1} alt='linkedin icon' /></a>
-                            <a href='https://github.com/Salissa4'><img src={github} alt='github icon' /></a>
+                            <a href='https://github.com/Salissa4'><img src={git2} alt='github icon' /></a>
+                            <a href={Pdf} target="_black"><img src={resume} alt='document'/></a>
+                            <a href="mailto:salissa4@gmail.com?"><img src={email} alt='envelope'/></a>
                         </div>
                         <p>Copyright 2022. All Rights Reserved</p>
                     </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,10 @@ import Navbar from 'react-bootstrap/Navbar';
 import { BrowserRouter as Router } from "react-router-dom";
 import sal2 from '../assets/images/sal2.svg';
 import navIcon1 from '../assets/images/nav-icon1.svg';
-import github from '../assets/images/github.png';
+import git2 from '../assets/images/git2.png';
+import Pdf from '../assets/docs/SH.pdf';
+import resume from '../assets/images/resume.png';
+import email from '../assets/images/email.png'
 
 export default function NavBar () {
 
@@ -43,12 +46,13 @@ export default function NavBar () {
               <Nav.Link href='#home' className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href='#skills' className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-              <Nav.Link href='#resume' className={activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('resume')}>Resume</Nav.Link>
             </Nav>
-            <span className='navbar-text'>
+            <span className='navbar-text' >
               <div className='social-icon'>
               <a href='https://www.linkedin.com/in/salissah/'><img src={navIcon1} alt='linked in icon' /></a>
-              <a href='https://github.com/Salissa4'><img src={github} alt='github icon' /></a>
+              <a href='https://github.com/Salissa4'><img src={git2} alt='github icon' /></a>
+              <a href={Pdf} target="_black"><img src={resume} alt='document'/></a>
+              <a href="mailto:salissa4@gmail.com?"><img src={email} alt='envelope'/></a>
               </div>
             </span>
           </Navbar.Collapse>
